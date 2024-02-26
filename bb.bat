6ce84699a3c861b9601f26a1ae8da57e1e39e394
yaguexx <161321598+yaguexx@users.noreply.github.com>
2024-02-26 13:49:56 +0100

    Update bb.bat

--- bb.bat
+++ bb.bat
@@ -1,13 +1,1 @@
-@echo off
-setlocal enabledelayedexpansion
 
-set "primerdirectorio=C:\Users\Alumno 24\Desktop\ISO
-set "segundodirectorio=C:\Users\Alumno 24\Desktop
-
-for %%F in ("%primerdirectorio%\*.txt") do (
-copy "%%F" "%segundodirectorio%"
-echo Archivo copiado: %%~nxF
-)
-endlocal
-
-pause
